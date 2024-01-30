f38d917ea391f4f8983f5cbf60dbff02d549139b
sabir <sabir@sabirs-MacBook-Air.local>
2024-01-30 15:12:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1 style={{ color: "blue" }} className="div">
+        Hello World!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
